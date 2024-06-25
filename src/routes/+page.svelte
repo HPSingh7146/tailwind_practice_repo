d716d991185e7e637aa0c9bee26d888462150918
hpsingh7146 <hpsingh7146@gmail.com>
2024-06-25 22:24:03 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="m-3 h-svh">
+	<h1 class="text-3xl font-bold mb-2 text-white">First Tailwind CSS</h1>
+	<h2 class="text-2xl font-bold text-white">This is a Subtitle</h2>
+	<div class="peragraph grid grid-cols-3 gap-4 h-auto text-white my-3">
+		<p class=" bg-red-400 rounded-3xl p-3 h-auto col-span-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta asperiores voluptatibus autem
+			qui? Quos consequuntur asperiores obcaecati architecto ratione nam magnam similique tempore
+			consectetur harum. Omnis excepturi tempore fugiat ducimus. Lorem ipsum dolor sit amet
+			consectetur, adipisicing elit. Accusamus aliquid voluptates, nobis assumenda repellendus culpa
+			aliquam alias ipsam quod soluta laboriosam! Officiis sed autem libero, enim iure quod id
+			tempora? Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero autem mollitia vitae ut
+			quam, eveniet explicabo ipsam magnam? Sunt, distinctio? Saepe, nemo? Dolores harum doloremque
+			libero sint beatae nulla culpa? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+			Ipsum incidunt omnis odit numquam hic non, ratione quaerat ex veniam adipisci velit
+			repellendus? Deleniti dolore aut neque labore rerum voluptate cum.
+		</p>
+		<p class=" bg-blue-400 rounded-3xl p-3">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam fuga odit ullam molestias
+			dolorum incidunt velit deserunt tenetur, adipisci beatae dignissimos cum iure corporis numquam
+			itaque maiores recusandae harum minima. Lorem ipsum, dolor sit amet consectetur adipisicing
+			elit. Distinctio cupiditate, laborum harum saepe odio veniam vitae quae eius facilis aliquam
+			nemo cumque repudiandae placeat id sunt ipsa, dolorum hic fugit.
+		</p>
+		<div class="squers grid grid-cols-3 gap-4">
+			<div class="yellow bg-green-600 h-80 rounded-3xl"></div>
+			<div class="yellow bg-pink-600 h-80 rounded-3xl"></div>
+			<div class="yellow bg-sky-600 h-80 rounded-3xl"></div>
+		</div>
+		<div class="yellow bg-blue-950 h-80 rounded-3xl col-span-2"></div>
+	</div>
+	<div class="inputops m-3">
+		<input type="text" placeholder="Type here" class=" bg-red-800 p-3 rounded-xl mr-3" />
+		<input type="checkbox" name="" id="" class=" accent-red-600" />
+		<label class=" text-teal-700 text-xl">hii</label>
+	</div>
+	<div class="buttons flex justify-around">
+		<button>Button 1</button>
+		<button>button 2</button>
+		<button>Button 3</button>
+	</div>
+</div>
+
+<slot />
